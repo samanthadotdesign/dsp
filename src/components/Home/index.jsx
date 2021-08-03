d8ae55390b5ac6189873797ed0cbb4f830bfe32f
samanthadotdesign <samleeml@gmail.com>
2021-08-03 22:21:56 +0800

    added a simple description on homepage

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -1,7 +1,8 @@
-import React, { useState, useEffect } from 'react';
-import { GlobalStyle } from '../../styles.js';
+import React from 'react';
+import { GlobalStyle, P } from '../../styles.js';
 import Modal from '../Modal/index.jsx';
 import { SignUpForm, LogInForm } from '../Form/index.jsx';
+import { Section, Em } from './styles.js';
 
 export default function Home({
   showLogInModal,
@@ -13,6 +14,16 @@ export default function Home({
   return (
     <>
       <GlobalStyle />
+      <Section>
+        <P>
+          <Em>Designer Starter Pack</Em>
+          {' '}
+          is a curated list of Figma skills by Samantha Lee of Funding Societies.
+          <br />
+          <br />
+          This list exists to better organize the list of skills I've come to know, like, and/or find interesting while working in product design.
+        </P>
+      </Section>
       {showSignUpModal && (
       <Modal toggleModal={toggleSignUpModal}>
         <SignUpForm
---
+++ src/components/Home/styles.js
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+export const Section = styled.section`
+  margin-top: 48px;
+  display: flex;
+  max-width: 350px;
+  justify-content: center;
+  padding: 24px;
+`;
+
+export const Em = styled.em`
+  font-family: 'Atkinson Bold';
+  font-style: normal;
+  font-size: 16px;
+`;
